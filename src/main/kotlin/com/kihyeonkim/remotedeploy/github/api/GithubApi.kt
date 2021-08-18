816a697e4c8370de088dc6c0632a632b6b4a9809
Kihyeon Kim <kiheyunkim@gmail.com>
2021-08-18 21:21:22 +0900

    [AutoDeployServer-#5] 젠킨스용 RSA 키 생성 로직 추가

--- src/main/kotlin/com/kihyeonkim/remotedeploy/github/api/GithubApi.kt
+++ src/main/kotlin/com/kihyeonkim/remotedeploy/github/api/GithubApi.kt
@@ -2,11 +2,11 @@ package com.kihyeonkim.remotedeploy.github.api
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.jcraft.jsch.JSch
+import com.jcraft.jsch.KeyPair
 import com.kihyeonkim.remotedeploy.github.model.BranchInfo
 import com.kihyeonkim.remotedeploy.github.model.RepositoryInfo
 import com.kihyeonkim.remotedeploy.repo.model.GithubKeySet
-import com.kihyeonkim.remotedeploy.repo.model.RepoInfo
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
@@ -15,6 +15,8 @@ import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponents
 import org.springframework.web.util.UriComponentsBuilder
+import java.io.ByteArrayOutputStream
+import java.security.SecureRandom
 
 /**
  * IDE : IntelliJ IDEA
@@ -28,6 +30,7 @@ class GithubApi {
 	private val repositoryBranchListApi: (String, String) -> String = { userName, repositoryName ->
 		"https://api.github.com/repos/${userName}/${repositoryName}/branches"
 	}
+	private val secureRandom: SecureRandom = SecureRandom()
 
 	fun getRepositoryList(accessToken: String): ArrayList<RepositoryInfo> {
 		val restTemplate = RestTemplate()
@@ -71,5 +74,33 @@ class GithubApi {
 		val itemType = object : TypeToken<ArrayList<BranchInfo>>() {}.type
 
 		return Gson().fromJson(responseEntity.body, itemType)
+	}
+
+	fun addDeployKeyAndGetPrivateKey(githubKeySet: GithubKeySet, repositoryName: String): String {
+		val restTemplate = RestTemplate()
+		val httpHeaders = HttpHeaders()
+		httpHeaders.contentType = MediaType(MediaType.APPLICATION_JSON, Charsets.UTF_8)
+		httpHeaders.add("Authorization", "token ${githubKeySet.accessToken}")
+
+		val generatedRSAKeySet = generateRSAKeySet()
+
+
+
+
+		return ""
+	}
+
+	fun generateRSAKeySet(): Pair<String, String> {
+		val bytePrivateKey = ByteArrayOutputStream()
+		val bytePublicKey = ByteArrayOutputStream()
+
+		val keyPair: KeyPair = KeyPair.genKeyPair(JSch(), KeyPair.RSA, 2048)
+		keyPair.writePrivateKey(bytePrivateKey, "".toByteArray())
+		keyPair.writePublicKey(bytePublicKey, "")
+
+		println(bytePrivateKey.toString())
+		println(bytePublicKey.toString())
+
+		return Pair(bytePrivateKey.toString(), bytePublicKey.toString())
 	}
 }

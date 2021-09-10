10225f864bd659172e216e913e368e5bb7ee1deb
Kihyeon Kim <kiheyunkim@gmail.com>
2021-09-11 02:25:21 +0900

    [AutoDeployServer-#16] Scm 정보 CRUD Controller 추가

--- src/main/kotlin/com/kihyeonkim/remotedeploy/scm/controller/ScmController.kt
+++ src/main/kotlin/com/kihyeonkim/remotedeploy/scm/controller/ScmController.kt
@@ -2,6 +2,7 @@ package com.kihyeonkim.remotedeploy.scm.controller
 
 import com.kihyeonkim.remotedeploy.common.response.DeployResponse
 import com.kihyeonkim.remotedeploy.scm.service.ScmService
+import com.kihyeonkim.remotedeploy.scm.vo.ScmVo
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
@@ -17,18 +18,42 @@ class ScmController(private val scmService: ScmService) {
 
 	@GetMapping("/keyAliasList")
 	@ResponseBody
-	fun addScm():DeployResponse<*>{
+	fun getKeyAliasList(): DeployResponse<*> {
 		return scmService.getKeyAliasList()
 	}
 
 	@GetMapping("/repositoryList")
 	@ResponseBody
-	fun getScmList(
+	fun getRepositoryList(
 		@RequestParam(
 			name = "scmInfoAlias",
 			required = true
 		) scmInfoAlias: String
 	): DeployResponse<*> {
 		return scmService.getRepositoryList(scmInfoAlias)
+	}
+
+	@GetMapping("/list")
+	@ResponseBody
+	fun getScmList(@RequestParam(name = "page", required = true, defaultValue = "1") page: Int): DeployResponse<*> {
+		return scmService.getScmList(page)
+	}
+
+	@PostMapping("/add")
+	@ResponseBody
+	fun postAddScm(@RequestBody scmVo: ScmVo): DeployResponse<*> {
+		return scmService.addScm(scmVo)
+	}
+
+	@PostMapping("/update")
+	@ResponseBody
+	fun postUpdateScm(@RequestBody scmVo: ScmVo): DeployResponse<*> {
+		return scmService.addScm(scmVo)
+	}
+
+	@DeleteMapping("/delete")
+	@ResponseBody
+	fun deleteScm(@RequestBody scmAlias: String): DeployResponse<*>{
+		return scmService.deleteScm(scmAlias);
 	}
 }

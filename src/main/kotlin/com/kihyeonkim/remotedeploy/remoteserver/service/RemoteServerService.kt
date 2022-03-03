067124ddea75e75a4a795267cc787a151a7844a9
kiheyunkim <kiheyunkim@gmail.com>
2022-03-04 00:30:44 +0900

    [AutoDeployServer-#19] remote Server Controller Swagger-Ui 추가

--- src/main/kotlin/com/kihyeonkim/remotedeploy/remoteserver/service/RemoteServerService.kt
+++ src/main/kotlin/com/kihyeonkim/remotedeploy/remoteserver/service/RemoteServerService.kt
@@ -2,8 +2,7 @@ package com.kihyeonkim.remotedeploy.remoteserver.service
 
 import com.kihyeonkim.remotedeploy.common.response.DeployResponse
 import com.kihyeonkim.remotedeploy.remoteserver.mapper.RemoteServerMapper
-import com.kihyeonkim.remotedeploy.remoteserver.mdoel.RemoteServerModel
-import com.kihyeonkim.remotedeploy.remoteserver.vo.RemoteServerInfoVo
+import com.kihyeonkim.remotedeploy.remoteserver.model.RemoteServerInfo
 import org.apache.commons.lang.StringUtils
 import org.springframework.stereotype.Service
 import java.util.regex.Pattern
@@ -24,39 +23,32 @@ class RemoteServerService(private val remoteServerMapper: RemoteServerMapper) {
 	fun getRemoteServerConnectionInfo(remoteServerAlias: String): DeployResponse<*> {
 		val queryResult = remoteServerMapper.selectRemoteServerConnectionInfo(remoteServerAlias)
 
-		return if (queryResult == null) {
+		return if (queryResult != null) {
 			DeployResponse(queryResult)
 		} else {
 			DeployResponse(false, null, "존재하지 않는 remoteServerAlias입니다")
 		}
 	}
 
-	fun addRemoteServeInfo(remoteServerInfoVo: RemoteServerInfoVo): DeployResponse<*> {
-		if (StringUtils.isEmpty(remoteServerInfoVo.remoteServerAlias) ||
-			StringUtils.isEmpty(remoteServerInfoVo.remoteIp) ||
-			StringUtils.isEmpty(remoteServerInfoVo.publicKey)
+	fun addRemoteServeInfo(remoteServerInfo: RemoteServerInfo): DeployResponse<*> {
+		if (StringUtils.isEmpty(remoteServerInfo.remoteServerAlias) ||
+			StringUtils.isEmpty(remoteServerInfo.remoteIp) ||
+			StringUtils.isEmpty(remoteServerInfo.publicKey)
 		) {
 			return DeployResponse(null, null, "값이 누락되었습니다")
 		}
 
-
 		if (Pattern.compile("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\$\n")
-				.matcher(remoteServerInfoVo.remoteIp!!).matches()
+				.matcher(remoteServerInfo.remoteIp!!).matches()
 		) {
 			return DeployResponse(null, null, "올바르지 않은 IP주소입니다")
 		}
 
-		if (remoteServerMapper.selectRemoteServerAliasExist(remoteServerInfoVo.remoteServerAlias!!)) {
+		if (remoteServerMapper.selectRemoteServerAliasExist(remoteServerInfo.remoteServerAlias!!)) {
 			return DeployResponse(null, null, "이미 존재하는 Alias 입니다")
 		}
 
-		remoteServerMapper.insertRemoteServerInfo(
-			RemoteServerModel(
-				remoteServerInfoVo.remoteServerAlias!!,
-				remoteServerInfoVo.remoteIp!!,
-				remoteServerInfoVo.publicKey!!
-			)
-		)
+		remoteServerMapper.insertRemoteServerInfo(remoteServerInfo)
 
 		return DeployResponse(true)
 	}
@@ -64,6 +56,26 @@ class RemoteServerService(private val remoteServerMapper: RemoteServerMapper) {
 	fun deleteRemoteServeInfo(remoteServerAlias: String): DeployResponse<*> {
 		val deleteResult = remoteServerMapper.deleteRemoteServerInfo(remoteServerAlias)
 		return if (deleteResult > 0) {
+			DeployResponse(true)
+		} else {
+			DeployResponse(null, null, "삭제할 대상이 없습니다")
+		}
+	}
+
+	fun modifyRemoteServerInfo(remoteServerInfo: RemoteServerInfo): DeployResponse<*> {
+		if (StringUtils.isEmpty(remoteServerInfo.remoteServerAlias)) {
+			DeployResponse(false, null, "존재하지 않는 remoteServerAlias입니다")
+		}
+
+		if (StringUtils.isEmpty(remoteServerInfo.remoteServerAlias) ||
+			StringUtils.isEmpty(remoteServerInfo.remoteIp) ||
+			StringUtils.isEmpty(remoteServerInfo.publicKey)
+		) {
+			return DeployResponse(null, null, "값이 누락되었습니다")
+		}
+
+		val updateResult = remoteServerMapper.updateRemoteServerInfo(remoteServerInfo)
+		return if (updateResult > 0) {
 			DeployResponse(true)
 		} else {
 			DeployResponse(null, null, "삭제할 대상이 없습니다")

c654048b504371083fbe96530c9e571f9b345799
kiheyunkim <kiheyunkim@gmail.com>
2022-03-08 20:16:48 +0900

    [AutoDeployServer-#22] repository 컨트롤러 추가

--- src/main/kotlin/com/kihyeonkim/remotedeploy/repositories/controller/RepositoryController.kt
+++ src/main/kotlin/com/kihyeonkim/remotedeploy/repositories/controller/RepositoryController.kt
@@ -1,8 +1,11 @@
 package com.kihyeonkim.remotedeploy.repositories.controller
 
 import com.kihyeonkim.remotedeploy.common.response.DeployResponse
+import com.kihyeonkim.remotedeploy.repositories.model.Repository
 import com.kihyeonkim.remotedeploy.repositories.service.RepositoryService
-import com.kihyeonkim.remotedeploy.repositories.vo.ScmVo
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
 /**
@@ -11,43 +14,52 @@ import org.springframework.web.bind.annotation.*
  * GitHub : http://github.com/kiheyunkim
  * Comment :
  */
-//@Controller
+@Controller
+@Api("레포지토리 관리")
 @RequestMapping("/repository")
 class RepositoryController(private val repositoryService: RepositoryService) {
-/*
 	@GetMapping("/list")
+	@ApiOperation("레포지토리 목록 조회")
 	@ResponseBody
 	fun getRepositoryList(
 		@RequestParam(
 			name = "scmInfoAlias",
 			required = true
-		) scmInfoAlias: String
+		) scmInfoAlias: String,
+		@RequestParam(
+			name = "page", required = true, defaultValue = "1"
+		) page: Int
 	): DeployResponse<*> {
 		return repositoryService.getRepositoryList(scmInfoAlias)
 	}
 
-	@GetMapping("/list")
+	@GetMapping("/detail")
+	@ApiOperation("레포지토리 상세 조회")
 	@ResponseBody
-	fun getScmList(@RequestParam(name = "page", required = true, defaultValue = "1") page: Int): DeployResponse<*> {
-		return repositoryService.getScmList(page)
+	fun getRepositoryDetail(
+		@RequestParam(
+			name = "scmInfoAlias",
+			required = true
+		) scmInfoAlias: String
+	): DeployResponse<*> {
+		return repositoryService.getRepositoryDetail()
 	}
 
 	@PostMapping("/add")
 	@ResponseBody
-	fun postAddScm(@RequestBody scmVo: ScmVo): DeployResponse<*> {
-		return repositoryService.addScm(scmVo)
+	fun postAddScm(@RequestBody repository: Repository): DeployResponse<*> {
+		return repositoryService.addScm(repository)
 	}
 
 	@PostMapping("/update")
 	@ResponseBody
-	fun postUpdateScm(@RequestBody scmVo: ScmVo): DeployResponse<*> {
-		return repositoryService.addScm(scmVo)
+	fun postUpdateScm(@RequestBody repository: Repository): DeployResponse<*> {
+		return repositoryService.addScm(repository)
 	}
 
 	@DeleteMapping("/delete")
 	@ResponseBody
-	fun deleteScm(@RequestBody scmAlias: String): DeployResponse<*>{
+	fun deleteScm(@RequestBody scmAlias: String): DeployResponse<*> {
 		return repositoryService.deleteScm(scmAlias);
 	}
-	*/
 }
